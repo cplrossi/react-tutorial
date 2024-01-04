460042b46c53420c05d2fec5f0fcea7448fa80fc
Stefano Borsatto <cplrossi@scroc.co>
2024-01-04 17:23:01 +0100

    ok basic game

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
-	if (squares[i]) {
+	if (squares[i] || calculateWinner(squares)) {
 	    return;
 	}
 	
@@ -28,8 +28,18 @@ export default function Board() {
 	setSquares(nextSquares);
 	setXIsNext(!xIsNext);
     }
+
+    const winner = calculateWinner(squares);
+    let status;
+
+    if (winner) {
+	status = "Winner: " + winner;
+    } else {
+	status = "Next player: " + (xIsNext ? "X": "O");
+    }
     
     return <>
+<div className="status">{status}</div>
 <div className="board-row">
 <Square value={squares[0]} onClick={() => handleClick(0)} />
 <Square value={squares[1]} onClick={() => handleClick(1)} />
@@ -46,4 +56,27 @@ export default function Board() {
 <Square value={squares[8]} onClick={() => handleClick(8)} />
 </div>
     </>
+}
+
+function calculateWinner(squares) {
+    const lines = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+	const [a, b, c] = lines[i];
+
+	if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+	    return squares[a];
+	}
+    }
+
+    return null;
 }
